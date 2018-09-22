49ce48339b5db5519b57bb54b551d5649cfa03e1
Rafael <rc.tejon@uniandes.edu.co>
2018-09-22 17:08:13 -0500

    lamding page

--- FrontHackAmericas/src/App.vue
+++ FrontHackAmericas/src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <router-link to="/">Landing</router-link>
-    <hr>
-    <router-link to="/HelloWorld">World</router-link>
     <router-view></router-view>
   </div>
 </template>
@@ -62,7 +59,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 
 h1,
