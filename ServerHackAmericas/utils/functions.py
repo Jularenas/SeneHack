1cdea4ddf1deed9d0cee28a53a6dcfdf0a358468
Rafael <rc.tejon@uniandes.edu.co>
2018-09-22 06:56:58 -0500

    distance

--- ServerHackAmericas/utils/functions.py
+++ ServerHackAmericas/utils/functions.py
@@ -1,2 +1,19 @@
 import os
 import hashlib
+import math
+import json
+
+rootdir ='C:/Users/rtb15/Desktop/HackAmericas/SeneHack/ServerHackAmericas/utils/unificados.json'
+
+def distance(origin, destination):
+    lat1, lon1 = origin['latitud'],origin['longitud']
+    lat2, lon2 = destination['latitud'],destination['longitud']
+    radius = 6371 # km
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
