0e4c69288f8bfea8ad17da360a3a971cad565827
JaManrique <ja.manrique@uniandes.edu.co>
2018-09-22 22:49:37 -0500

    El buen $

--- FrontHackAmericas/src/components/Base/Boton.vue
+++ FrontHackAmericas/src/components/Base/Boton.vue
@@ -14,7 +14,7 @@
     computed: {
       listeners(){
         return{
-          ...this.listeners,
+          ...this.$listeners,
           input: event => this.$emit("input", event.target.value)
         }
       }
