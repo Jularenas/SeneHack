11219f3e418dafc4e2c918d928f593ff732ffdbc
Sergio G <s.guzmanm@uniandes.edu.co>
2018-09-23 08:12:48 -0500

    Script for generating visualizations for every point in taxis journeys from april to July

---
+++ ServerHackAmericas/utils/unificationManrique.py
@@ -0,0 +1,26 @@
+import os
+import json
+
+
+rootdir = 'C:/Users/Dell/Downloads/DatosTaxisBogotaY4PT/DatosTaxisBogotaY4PT/'
+unificados=[]
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        print(rootdir+str(file))
+        with open(rootdir+str(file), 'r') as f:
+            rutas = json.load(f)
+        for ruta in rutas:
+            if ruta.get('interrumpircarrera') is None and not ruta.get('fincarrera') is None:
+                unificado={}
+                unificado['id']=ruta['_id']['$oid']
+                unificado['recorrido']=ruta['recorrido']
+                unificado['destino']=ruta['destino']
+                unificado['origen']=ruta['origen']
+                unificado['fecha_calculo']=ruta['fecha_calculo']['$date']
+                unificados.append(unificado)
+with open('unificadosManrique.json', 'w') as fp:
+    json.dump(unificados, fp)    
+        
+                
+       
+
